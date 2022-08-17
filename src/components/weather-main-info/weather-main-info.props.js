7fd7922983ecabe349d69b10c1b615071d91e9d0
mukhammadyn <nurmuhammaddeveloper@gmail.com>
2022-08-17 07:53:13 +0500

    change mode

--- src/components/weather-main-info/weather-main-info.props.js
+++ src/components/weather-main-info/weather-main-info.props.js
@@ -8,7 +8,7 @@ export const useWeatherInfoProps = () => {
   let theme = "";
 
   if (clouds >= 10 && clouds <= 50) theme = "weather-main--grey";
-  else if (clouds > 50 && clouds <= 80) theme = "weather-main--black";
+  else if (clouds > 50 && clouds <= 100) theme = "weather-main--black";
   else theme = "";
 
   return {
